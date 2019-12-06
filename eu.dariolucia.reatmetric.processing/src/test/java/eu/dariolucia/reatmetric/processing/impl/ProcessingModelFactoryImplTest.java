d90a1b0dc1091b24a0612242854c4bef6b4e87eb
Dario <dario.lucia@gmail.com>
2019-12-06 19:23:34 +0100

    Updates

--- eu.dariolucia.reatmetric.processing/src/test/java/eu/dariolucia/reatmetric/processing/impl/ProcessingModelFactoryImplTest.java
+++ eu.dariolucia.reatmetric.processing/src/test/java/eu/dariolucia/reatmetric/processing/impl/ProcessingModelFactoryImplTest.java
@@ -8,7 +8,11 @@
 package eu.dariolucia.reatmetric.processing.impl;
 
 import eu.dariolucia.reatmetric.api.common.AbstractDataItem;
+import eu.dariolucia.reatmetric.api.model.AlarmState;
+import eu.dariolucia.reatmetric.api.model.SystemEntity;
 import eu.dariolucia.reatmetric.api.model.SystemEntityPath;
+import eu.dariolucia.reatmetric.api.parameters.ParameterData;
+import eu.dariolucia.reatmetric.api.parameters.Validity;
 import eu.dariolucia.reatmetric.processing.IProcessingModel;
 import eu.dariolucia.reatmetric.processing.IProcessingModelOutput;
 import eu.dariolucia.reatmetric.processing.ProcessingModelException;
@@ -21,7 +25,10 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProcessingModelFactoryImplTest {
 
@@ -68,7 +75,7 @@ class ProcessingModelFactoryImplTest {
         Logger testLogger = Logger.getLogger(getClass().getName());
         ProcessingDefinition pd = ProcessingDefinition.load(this.getClass().getClassLoader().getResourceAsStream("processing_definitions.xml"));
         ProcessingModelFactoryImpl factory = new ProcessingModelFactoryImpl();
-        List<AbstractDataItem> outList = new LinkedList<>();
+        List<AbstractDataItem> outList = new CopyOnWriteArrayList<>();
         // All output data items go in the outList
         IProcessingModelOutput output = outList::addAll;
         IProcessingModel model = factory.build(pd, output, null);
@@ -81,9 +88,30 @@ class ProcessingModelFactoryImplTest {
         model.injectParameters(Arrays.asList(batteryCurrent, batteryState, batteryTension));
 
         Thread.sleep(5000);
-        // TODO: Verify: 1001, 1002, 1003
-        System.out.println("List size: " + outList.size());
-        outList.forEach(System.out::println);
+
+        assertEquals(8, outList.size());
+        // Battery state
+        assertEquals(1001, ((ParameterData)outList.get(0)).getExternalId());
+        assertEquals("BATTERY_STATE", ((SystemEntity)outList.get(1)).getName());
+        assertEquals(true, ((ParameterData)outList.get(0)).getSourceValue());
+        assertEquals(true, ((ParameterData)outList.get(0)).getEngValue());
+        assertEquals(Validity.VALID, ((ParameterData)outList.get(0)).getValidity());
+        assertEquals(AlarmState.NOMINAL, ((ParameterData)outList.get(0)).getAlarmState());
+        // Battery tension
+        assertEquals(1002, ((ParameterData)outList.get(2)).getExternalId());
+        assertEquals("BATTERY_TENSION", ((SystemEntity)outList.get(3)).getName());
+        assertEquals(1000L, ((ParameterData)outList.get(2)).getSourceValue());
+        assertEquals(2010L, ((ParameterData)outList.get(2)).getEngValue());
+        assertEquals(Validity.VALID, ((ParameterData)outList.get(2)).getValidity());
+        assertEquals(AlarmState.NOMINAL, ((ParameterData)outList.get(2)).getAlarmState());
+        // Battery current
+        assertEquals(1003, ((ParameterData)outList.get(4)).getExternalId());
+        assertEquals("BATTERY_CURRENT", ((SystemEntity)outList.get(5)).getName());
+        // Battery state (enumeration)
+        assertEquals(1004, ((ParameterData)outList.get(6)).getExternalId());
+        assertEquals("BATTERY_STATE_EN", ((SystemEntity)outList.get(7)).getName());
+
+        testLogger.info("List size: " + outList.size());
 
         batteryState = ParameterSample.of(1001, false);
         batteryTension = ParameterSample.of(1002, 1100L);
