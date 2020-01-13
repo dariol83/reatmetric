88e9366cd4d3bbfce495c7e88d20d344e04e8a73
Dario <dario.lucia@gmail.com>
2020-01-13 21:44:19 +0100

    Raw data broker development

---
+++ eu.dariolucia.reatmetric.core/src/main/java/eu/dariolucia/reatmetric/core/configuration/DriverConfiguration.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2020.  Dario Lucia (dario.lucia@gmail.com)
+ * All rights reserved.
+ *
+ * Right to reproduce, use, modify and distribute (in whole or in part) this library for demonstrations/trainings/study/commercial purposes shall be granted by the author in writing.
+ */
+
+package eu.dariolucia.reatmetric.core.configuration;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class DriverConfiguration {
+
+    @XmlAttribute(name = "name", required = true)
+    private String name;
+
+    @XmlAttribute(name = "type", required = true)
+    private String type;
+
+    @XmlAttribute(name = "configuration", required = true)
+    private String configuration;
+
+    public DriverConfiguration() {
+    }
+
+    public DriverConfiguration(String name, String type, String configuration) {
+        this.name = name;
+        this.type = type;
+        this.configuration = configuration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public DriverConfiguration setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public DriverConfiguration setType(String type) {
+        this.type = type;
+        return this;
+    }
+
+    public String getConfiguration() {
+        return configuration;
+    }
+
+    public DriverConfiguration setConfiguration(String configuration) {
+        this.configuration = configuration;
+        return this;
+    }
+}
--- eu.dariolucia.reatmetric.core/src/main/java/eu/dariolucia/reatmetric/core/configuration/ServiceCoreConfiguration.java
+++ eu.dariolucia.reatmetric.core/src/main/java/eu/dariolucia/reatmetric/core/configuration/ServiceCoreConfiguration.java
@@ -7,6 +7,22 @@
 
 package eu.dariolucia.reatmetric.core.configuration;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.LinkedList;
+import java.util.List;
+
+@XmlRootElement(name = "core", namespace = "http://dariolucia.eu/reatmetric/core/configuration")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class ServiceCoreConfiguration {
+
+    @XmlElement(name = "archiveLocation", required = true)
+    private String archiveLocation;
+
+    @XmlElement(name = "driver", required = true)
+    private List<DriverConfiguration> drivers = new LinkedList<>();
+
 
 }
