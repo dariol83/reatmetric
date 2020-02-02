2dac4513e51b9bba6ccd5305cf37f92c177ccb55
Dario Lucia <dario.lucia@gmail.com>
2020-02-02 09:40:12 +0100

    POM updates

--- eu.dariolucia.reatmetric.driver.spacecraft/src/main/java/module-info.java
+++ eu.dariolucia.reatmetric.driver.spacecraft/src/main/java/module-info.java
@@ -2,9 +2,6 @@ module eu.dariolucia.reatmetric.driver.spacecraft {
 
     requires eu.dariolucia.reatmetric.api;
 
-    requires eu.dariolucia.sle.testtool;
     requires eu.dariolucia.ccsds.encdec;
-    requires eu.dariolucia.ccsds.encdec.s2k;
-    requires eu.dariolucia.ccsds.s2k.mib;
     requires eu.dariolucia.ccsds.tmtc;
 }
