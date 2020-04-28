1e5bbff93daae4dcd7dd5da581af2c7bdba180ff
Dario <dario.lucia@gmail.com>
2020-04-28 21:38:32 +0200

    Initial design of the activity dialog (progressing... slow)

--- eu.dariolucia.reatmetric.ui/src/main/java/eu/dariolucia/reatmetric/ui/controller/ActivityInvocationDialogController.java
+++ eu.dariolucia.reatmetric.ui/src/main/java/eu/dariolucia/reatmetric/ui/controller/ActivityInvocationDialogController.java
@@ -16,10 +16,13 @@
 
 package eu.dariolucia.reatmetric.ui.controller;
 
+import eu.dariolucia.reatmetric.api.activity.ActivityArgumentDescriptor;
 import eu.dariolucia.reatmetric.api.activity.ActivityDescriptor;
 import eu.dariolucia.reatmetric.api.common.Pair;
-import eu.dariolucia.reatmetric.api.processing.IActivityHandler;
 import eu.dariolucia.reatmetric.api.processing.input.ActivityRequest;
+import javafx.beans.property.ReadOnlyStringProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -37,7 +40,7 @@ public class ActivityInvocationDialogController implements Initializable {
     @FXML
     protected ChoiceBox<String> routeChoiceBox;
     @FXML
-    protected TableView argumentsTableView;
+    protected TableView<ArgumentBean> argumentsTableView;
     @FXML
     protected TableColumn nameColumn;
     @FXML
@@ -47,7 +50,7 @@ public class ActivityInvocationDialogController implements Initializable {
     @FXML
     protected TableColumn unitColumn;
     @FXML
-    protected TableView propertiesTableView;
+    protected TableView<PropertyBean> propertiesTableView;
     @FXML
     protected TableColumn keyColumn;
     @FXML
@@ -57,20 +60,102 @@ public class ActivityInvocationDialogController implements Initializable {
     @FXML
     protected Button cancelButton;
 
+    private ActivityDescriptor descriptor;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        argumentsTableView.setEditable(true);
+        nameColumn.setEditable(false);
     }
 
     public void initialiseActivityDialog(ActivityDescriptor descriptor, ActivityRequest currentRequest, List<Pair<String, Boolean>> routesWithAvailability) {
+        this.descriptor = descriptor;
+        activityLabel.setText(descriptor.getPath().asString());
+        descriptionLabel.setText(descriptor.getDescription());
+        // Set the routes
+        for(Pair<String,Boolean> route : routesWithAvailability) {
+            routeChoiceBox.getItems().add(route.getFirst());
+        }
+        // Set the selected route or default
+        if(currentRequest != null) {
+            routeChoiceBox.getSelectionModel().select(currentRequest.getRoute());
+        } else if(descriptor.getDefaultRoute() != null) {
+            routeChoiceBox.getSelectionModel().select(descriptor.getDefaultRoute());
+        }
+        // Set the arguments
+        for(ActivityArgumentDescriptor aad : descriptor.getArgumentDescriptors()) {
+            ArgumentBean ab = new ArgumentBean(aad);
+            argumentsTableView.getItems().add(ab);
+            if(currentRequest != null) {
+                updateArgument(ab, aad, currentRequest);
+            }
+        }
+        // Set the properties
+        for(Pair<String, String> property : descriptor.getProperties()) {
+            PropertyBean pb = new PropertyBean(property);
+            propertiesTableView.getItems().add(pb);
+            if(currentRequest != null) {
+                updateProperty(pb, property, currentRequest);
+            }
+        }
+    }
 
+    private void updateProperty(PropertyBean pb, Pair<String, String> property, ActivityRequest currentRequest) {
+        // TODO
+    }
+
+    private void updateArgument(ArgumentBean ab, ActivityArgumentDescriptor aad, ActivityRequest currentRequest) {
+        // TODO
     }
 
     private static class ArgumentBean {
 
+        private final ActivityArgumentDescriptor descriptor;
+        private final ReadOnlyStringProperty name;
+        private final ReadOnlyStringProperty unit;
+        private final SimpleObjectProperty<Object> rawValue;
+        private final SimpleObjectProperty<Object> engValue;
+
+        public ArgumentBean(ActivityArgumentDescriptor aad) {
+            descriptor = aad;
+            name = new SimpleStringProperty(aad.getName());
+            unit = new SimpleStringProperty(aad.getUnit());
+            rawValue = new SimpleObjectProperty<>();
+            engValue = new SimpleObjectProperty<>();
+            if(aad.isDefaultValuePresent()) {
+                if(aad.getRawDefaultValue() != null) {
+                    rawValue.set(aad.getRawDefaultValue());
+                } else if(aad.getEngineeringDefaultValue() != null) {
+                    engValue.set(aad.getEngineeringDefaultValue());
+                }
+            }
+        }
+
+        public ActivityArgumentDescriptor getDescriptor() {
+            return descriptor;
+        }
+
+        public ReadOnlyStringProperty nameProperty() {
+            return name;
+        }
+
+        public ReadOnlyStringProperty unitProperty() {
+            return unit;
+        }
+
+        public SimpleObjectProperty<Object> rawValueProperty() {
+            return rawValue;
+        }
+
+        public SimpleObjectProperty<Object> engValueProperty() {
+            return engValue;
+        }
     }
 
     private static class PropertyBean {
 
+        public PropertyBean(Pair<String, String> property) {
+
+        }
     }
 }
