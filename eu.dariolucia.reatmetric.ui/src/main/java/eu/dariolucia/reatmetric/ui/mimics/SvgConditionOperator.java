d637b3ef4210d67221024ebe7ee7138d0c509f7e
Dario Lucia <dario.lucia@gmail.com>
2021-02-13 13:14:37 +0100

    Handling long-int-double mixed comparison

--- eu.dariolucia.reatmetric.ui/src/main/java/eu/dariolucia/reatmetric/ui/mimics/SvgConditionOperator.java
+++ eu.dariolucia.reatmetric.ui/src/main/java/eu/dariolucia/reatmetric/ui/mimics/SvgConditionOperator.java
@@ -33,19 +33,62 @@ public enum SvgConditionOperator implements BiFunction<Object, Object, Boolean> 
     public Boolean apply(Object o, Object o2) {
         switch(this) {
             case EQ:
-                return Objects.equals(o, o2);
+                return innerEquals(o, o2);
             case NQ:
-                return !Objects.equals(o, o2);
+                return !innerEquals(o, o2);
             case LTE:
-                return Objects.equals(o, o2) || ValueUtil.compare(o, o2) < 0;
+                return innerEquals(o, o2) || innerCompare(o, o2) < 0;
             case GTE:
-                return Objects.equals(o, o2) || ValueUtil.compare(o, o2) > 0;
+                return innerEquals(o, o2) || innerCompare(o, o2) > 0;
             case LT:
-                return ValueUtil.compare(o, o2) < 0;
+                return innerCompare(o, o2) < 0;
             case GT:
-                return ValueUtil.compare(o, o2) > 0;
+                return innerCompare(o, o2) > 0;
             default:
                 throw new IllegalStateException("Missing support for operator " + this);
+        }
+    }
+
+    private boolean innerEquals(Object o1, Object o2) {
+        if(o1 instanceof Number) {
+            // Assume that also o2 is a number
+            Number n1 = (Number) o1;
+            Number n2 = (Number) o2;
+            if(n1 instanceof Double || n2 instanceof Double) {
+                return n1.doubleValue() == n2.doubleValue();
+            } else if(n1 instanceof Long || n2 instanceof Long) {
+                return n1.longValue() == n2.longValue();
+            } else {
+                return n1.intValue() == n2.intValue();
+            }
+        } else {
+            return Objects.equals(o1, o2);
+        }
+    }
+
+    /**
+     * When comparing boxed numbers (long, int, double), the standard equality and compare do not work well.
+     *
+     * @param o1 the first object to compare
+     * @param o2 the second object to compare
+     * @return -1 if o1 is before o2, 1 if o1 is after o2, 0 if equal
+     */
+    private int innerCompare(Object o1, Object o2) {
+        if(o1 instanceof Number) {
+            // Assume that also o2 is a number
+            Number n1 = (Number) o1;
+            Number n2 = (Number) o2;
+            if(n1 instanceof Double || n2 instanceof Double) {
+                double result = n1.doubleValue() - n2.doubleValue();
+                return result == 0 ? 0 : (result < 0 ? -1 : 1);
+            } else if(n1 instanceof Long || n2 instanceof Long) {
+                long result = n1.longValue() - n2.longValue();
+                return result == 0 ? 0 : (result < 0 ? -1 : 1);
+            } else {
+                return n1.intValue() - n2.intValue();
+            }
+        } else {
+            return ValueUtil.compare(o1, o2);
         }
     }
 }
