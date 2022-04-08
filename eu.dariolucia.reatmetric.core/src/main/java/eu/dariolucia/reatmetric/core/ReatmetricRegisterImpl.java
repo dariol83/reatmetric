446e4ee29955c728e407d644d89fa39c209f1bac
Dario Lucia <dario.lucia@gmail.com>
2022-04-08 19:12:18 +0200

    Fixed NullPointerException in case of missing configuration file

--- eu.dariolucia.reatmetric.core/src/main/java/eu/dariolucia/reatmetric/core/ReatmetricRegisterImpl.java
+++ eu.dariolucia.reatmetric.core/src/main/java/eu/dariolucia/reatmetric/core/ReatmetricRegisterImpl.java
@@ -22,11 +22,15 @@ import eu.dariolucia.reatmetric.api.common.exceptions.ReatmetricException;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Implementation of a registry that returns a single system (configured via system property).
  */
 public class ReatmetricRegisterImpl implements IReatmetricRegister {
+
+    private static final Logger LOG = Logger.getLogger(ReatmetricRegisterImpl.class.getName());
 
     private static final String INIT_FILE_KEY = "reatmetric.core.config"; // Absolute location of the configuration file, to configure the core instance
 
@@ -36,7 +40,13 @@ public class ReatmetricRegisterImpl implements IReatmetricRegister {
     public List<IReatmetricSystem> availableSystems() throws ReatmetricException {
         synchronized (ReatmetricRegisterImpl.class) {
             if(system == null) {
-                system = new ReatmetricSystemImpl(System.getProperty(INIT_FILE_KEY));
+                String fileLocation = System.getProperty(INIT_FILE_KEY);
+                if(fileLocation == null || fileLocation.isBlank()) {
+                    LOG.log(Level.INFO, "Init property key " + INIT_FILE_KEY + " for Reatmetric Core implementation not present: skipping");
+                    return Collections.emptyList();
+                } else {
+                    system = new ReatmetricSystemImpl(fileLocation);
+                }
             }
             return Collections.singletonList(system);
         }
