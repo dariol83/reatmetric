026fab6de5b6251bd9ae7774596dd694d0527104
Dario Lucia <dario.lucia@gmail.com>
2023-03-27 18:16:03 +0200

    Added assembly building for Linux systems (Windows generates errors due to too long file names)

---
+++ eu.dariolucia.reatmetric.ui.spacecraft/scripts/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java --module-path="bin" -Dreatmetric.core.config=<PATH TO CORE CONFIGURATION XML> --add-exports javafx.base/com.sun.javafx.event=org.controlsfx.controls -m eu.dariolucia.reatmetric.ui/eu.dariolucia.reatmetric.ui.ReatmetricUI
